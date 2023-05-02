069fce67cce5d8e33ca01f8a23d5a53f50c881f0
Federico <federuedaleal@hotmail.com>
2023-05-01 23:49:35 -0300

    Agregué el texto correspondiente a cada card y modifique la función del buscador para que se actualice en tiempo real

--- js/buscador.js
+++ js/buscador.js
@@ -3,24 +3,36 @@ let inputSearch = document.getElementById('inputBuscador')
 let btnBuscador = document.getElementById('btnBuscarArticulo')
 let formularioBuscador = document.getElementById('formBuscador')
 let contenedorArticulo = document.getElementById('contenedorArticulos')
-//manejador de eventos
-formularioBuscador.addEventListener('submit', buscarArticulo);
+let mensajeBusqueda = document.createElement('p');
+mensajeBusqueda.innerHTML = "No se encontró el artículo que buscas";
+mensajeBusqueda.classList.add("d-none");
+contenedorArticulo.appendChild(mensajeBusqueda);
 
-function buscarArticulo(e) {
-    e.preventDefault();
-    const inputBuscador = document.querySelector('#inputBuscador');
-    const textoBusqueda = inputBuscador.value.trim().toLowerCase();
-    let parrafo = document.createElement("p");
-    const articulos = document.querySelectorAll('#contenedorArticulos .card-title');
-    articulos.forEach(articulo => {
+//manejador de eventos
+inputSearch.addEventListener('input', buscarArticulo);
+
+function buscarArticulo() {
+  realizarBusqueda();
+}
+
+function realizarBusqueda() {
+  const inputBuscador = document.querySelector('#inputBuscador');
+  const textoBusqueda = inputBuscador.value.trim().toLowerCase();
+  let encontrados = false;
+  const articulos = document.querySelectorAll('#contenedorArticulos .card-title');
+  articulos.forEach(articulo => {
       const tituloArticulo = articulo.textContent.trim().toLowerCase();
       const cardArticulo = articulo.closest('#cardArticulo');
-  
       if (tituloArticulo.includes(textoBusqueda)) {
-        cardArticulo.classList.remove('d-none');        
+          cardArticulo.classList.remove('d-none');
+          encontrados = true;
       } else {
-        cardArticulo.classList.add('d-none');
-        parrafo.innerHTML='No se encontró el artículo que buscas'
+          cardArticulo.classList.add('d-none');        
       }
-    });
+  });
+  if (!encontrados) {
+      mensajeBusqueda.classList.remove("d-none");
+  } else {
+      mensajeBusqueda.classList.add("d-none");
   }
+}
